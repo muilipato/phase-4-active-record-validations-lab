c651cec7d3bb91991f3fa339c4389c2420ed0ac8
Patrick <muilipato@gmail.com>
2023-07-07 18:02:19 +0300

    added the validations in post.rb and done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :title_contains_clickbait_phrases
+
+  private
+
+  def title_contains_clickbait_phrases
+    unless title.present? && title.match?(/(Won't Believe|Secret|Top \d|Guess)/)
+      errors.add(:title, 'must contain clickbait phrases')
+    end
+  end
 end
